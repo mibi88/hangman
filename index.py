df0dd701fd909ee009e803dcdee4f3707a7ff9b3
mibi88 <76903855+mibi88@users.noreply.github.com>
2021-04-18 11:56:16 +0200

    v.0.2-simp

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ from random import *
 main = Tk()
 main.title("Hangman game")
 chooseframe = LabelFrame(main, text="Start a new game")
-chooseframe.pack(fill="both")
+chooseframe.pack(fill="both", expand = True)
 #=================variables=================
 dicoadress = StringVar("")
 word = StringVar("")
@@ -16,6 +16,7 @@ resultword = StringVar("")
 wordlen = IntVar()
 nbtry = IntVar()
 letterspressed = StringVar("")
+imgvar = StringVar("")
 #=========================================
 
 #=========================================
@@ -23,6 +24,113 @@ letterspressed = StringVar("")
 #=========================================
 
 #================commands================
+# Bind this letters
+def letterbind():
+    main.bind("a",acom)
+    main.bind("b",bcom)
+    main.bind("c",ccom)
+    main.bind("d",dcom)
+    main.bind("e",ecom)
+    main.bind("f",fcom)
+    main.bind("g",gcom)
+    main.bind("h",hcom)
+    main.bind("i",icom)
+    main.bind("j",jcom)
+    main.bind("k",kcom)
+    main.bind("l",lcom)
+    main.bind("m",mcom)
+    main.bind("n",ncom)
+    main.bind("o",ocom)
+    main.bind("p",pcom)
+    main.bind("q",qcom)
+    main.bind("r",rcom)
+    main.bind("s",scom)
+    main.bind("t",tcom)
+    main.bind("u",ucom)
+    main.bind("v",vcom)
+    main.bind("w",wcom)
+    main.bind("x",xcom)
+    main.bind("y",ycom)
+    main.bind("z",zcom)
+    main.bind("A",acom)
+    main.bind("B",bcom)
+    main.bind("C",ccom)
+    main.bind("D",dcom)
+    main.bind("E",ecom)
+    main.bind("F",fcom)
+    main.bind("G",gcom)
+    main.bind("H",hcom)
+    main.bind("I",icom)
+    main.bind("J",jcom)
+    main.bind("K",kcom)
+    main.bind("L",lcom)
+    main.bind("M",mcom)
+    main.bind("N",ncom)
+    main.bind("O",ocom)
+    main.bind("P",pcom)
+    main.bind("Q",qcom)
+    main.bind("R",rcom)
+    main.bind("S",scom)
+    main.bind("T",tcom)
+    main.bind("U",ucom)
+    main.bind("V",vcom)
+    main.bind("W",wcom)
+    main.bind("X",xcom)
+    main.bind("Y",ycom)
+    main.bind("Z",zcom)
+def unbind_ev():
+    main.unbind("a")
+    main.unbind("b")
+    main.unbind("c")
+    main.unbind("d")
+    main.unbind("e")
+    main.unbind("f")
+    main.unbind("g")
+    main.unbind("h")
+    main.unbind("i")
+    main.unbind("j")
+    main.unbind("k")
+    main.unbind("l")
+    main.unbind("m")
+    main.unbind("n")
+    main.unbind("o")
+    main.unbind("p")
+    main.unbind("q")
+    main.unbind("r")
+    main.unbind("s")
+    main.unbind("t")
+    main.unbind("u")
+    main.unbind("v")
+    main.unbind("w")
+    main.unbind("x")
+    main.unbind("y")
+    main.unbind("z")
+    main.unbind("A")
+    main.unbind("B")
+    main.unbind("C")
+    main.unbind("D")
+    main.unbind("E")
+    main.unbind("F")
+    main.unbind("G")
+    main.unbind("H")
+    main.unbind("I")
+    main.unbind("J")
+    main.unbind("K")
+    main.unbind("L")
+    main.unbind("M")
+    main.unbind("N")
+    main.unbind("O")
+    main.unbind("P")
+    main.unbind("Q")
+    main.unbind("R")
+    main.unbind("S")
+    main.unbind("T")
+    main.unbind("U")
+    main.unbind("V")
+    main.unbind("W")
+    main.unbind("X")
+    main.unbind("Y")
+    main.unbind("Z")
 def run():
    gameover.pack_forget()
    nbtry.set(10)
@@ -57,19 +165,35 @@ def run():
       selword = selword.replace("\n", "", 1)
    selword = selword.upper()
    len_selword = len(selword)
-   word.set(selword)
-   wordlen.set(len_selword)
    scroll = 0
-   resvar = ""
    while scroll != len_selword:
-      resvar += "*"
+      letter = selword[scroll:scroll + 1]
+      totrypossibleletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      if letter in totrypossibleletters:
+         baddico = False
+      else:
+         errormsg = "Bad dictionnary\nBad letter \"" + letter + "\".\nError number 2"
+         showerror("Error ...", errormsg)
+         baddico = True
+         break
       scroll += 1
-   resultword.set(resvar)
-   #print(word.get())
-   print(wordlen.get())
-   print(nbtry.get())
-   chooseframe.pack_forget()
-   mainframe.pack()
+   if baddico == False:
+      len_selword = len(selword)
+      word.set(selword)
+      wordlen.set(len_selword)
+      scroll = 0
+      resvar = ""
+      while scroll != len_selword:
+         resvar += "*"
+         scroll += 1
+      resultword.set(resvar)
+      #print(word.get())
+      print(wordlen.get())
+      print(nbtry.get())
+      chooseframe.pack_forget()
+      mainframe.pack(fill = "both", expand = True)
+      #hangman_drawing.pack(side = RIGHT, fill = "both", expand=True)
+      letterbind()
 def tryletter(letter):
    usedletters = letterspressed.get()
    if not letter in usedletters:
@@ -106,57 +230,57 @@ def tryletter(letter):
       letterspressed.set(usedletters)
    else:
          showinfo("Info ...", "Already tested letter.")
-def acom():
+def acom(event=None):
    tryletter("A")
-def bcom():
+def bcom(event=None):
    tryletter("B")
-def ccom():
+def ccom(event=None):
    tryletter("C")
-def dcom():
+def dcom(event=None):
    tryletter("D")
-def ecom():
+def ecom(event=None):
    tryletter("E")
-def fcom():
+def fcom(event=None):
    tryletter("F")
-def gcom():
+def gcom(event=None):
    tryletter("G")
-def hcom():
+def hcom(event=None):
    tryletter("H")
-def icom():
+def icom(event=None):
    tryletter("I")
-def jcom():
+def jcom(event=None):
    tryletter("J")
-def kcom():
+def kcom(event=None):
    tryletter("K")
-def lcom():
+def lcom(event=None):
    tryletter("L")
-def mcom():
+def mcom(event=None):
    tryletter("M")
-def ncom():
+def ncom(event=None):
    tryletter("N")
-def ocom():
+def ocom(event=None):
    tryletter("O")
-def pcom():
+def pcom(event=None):
    tryletter("P")
-def qcom():
+def qcom(event=None):
    tryletter("Q")
-def rcom():
+def rcom(event=None):
    tryletter("R")
-def scom():
+def scom(event=None):
    tryletter("S")
-def tcom():
+def tcom(event=None):
    tryletter("T")
-def ucom():
+def ucom(event=None):
    tryletter("U")
-def vcom():
+def vcom(event=None):
    tryletter("V")
-def wcom():
+def wcom(event=None):
    tryletter("W")
-def xcom():
+def xcom(event=None):
    tryletter("X")
-def ycom():
+def ycom(event=None):
    tryletter("Y")
-def zcom():
+def zcom(event=None):
    tryletter("Z")
 def game_over():
    wordvar = word.get()
@@ -170,19 +294,23 @@ def win():
 
 #==================frame==================
 mainframe = LabelFrame(main, text="Hangman game")
-mainframe.pack(fill="both")
+mainframe.pack(fill="both", expand = True)
 #:::
 def go_back():
    mainframe.pack_forget()
-   chooseframe.pack(fill="both")
+   chooseframe.pack(fill="both", expand = True)
+   unbind_ev()
 #:::
-wordl = Label(mainframe, textvariable = resultword)
+leftframe = Frame(mainframe, relief=FLAT)
+leftframe.pack(fill="both", expand = True, side = LEFT)
+
+wordl = Label(leftframe, textvariable = resultword)
 wordl.pack()
 #:::
-tryf = Frame(mainframe, relief=FLAT)
+tryf = Frame(leftframe, relief=FLAT)
 tryf.pack(fill="both")
 #---
-gameover = Label(mainframe, text = "Game Over !")
+gameover = Label(leftframe, text = "Game Over !")
 gameover.pack()
 #---
 info1 = Label(tryf, text="Remaining tests :")
@@ -191,107 +319,109 @@ trynb_l = Label(tryf, textvariable = nbtry)
 trynb_l.pack(side=LEFT)
 #:::
 #frames :
-line1 = Frame(mainframe, relief=FLAT)
-line1.pack(fill="both")
+line1 = Frame(leftframe, relief=FLAT)
+line1.pack(fill="both", expand = True)
 #---
-line2 = Frame(mainframe, relief=FLAT)
-line2.pack(fill="both")
+line2 = Frame(leftframe, relief=FLAT)
+line2.pack(fill="both", expand = True)
 #---
-line3 = Frame(mainframe, relief=FLAT)
-line3.pack(fill="both")
+line3 = Frame(leftframe, relief=FLAT)
+line3.pack(fill="both", expand = True)
 #---
-line4 = Frame(mainframe, relief=FLAT)
-line4.pack(fill="both")
+line4 = Frame(leftframe, relief=FLAT)
+line4.pack(fill="both", expand = True)
 #---
-line5 = Frame(mainframe, relief=FLAT)
-line5.pack(fill="both")
+line5 = Frame(leftframe, relief=FLAT)
+line5.pack(fill="both", expand = True)
 #buttons
 #=A=
 abutt = Button(line1, text="A", command=acom)
-abutt.pack(fill="both", side=LEFT)
+abutt.pack(side=LEFT, fill="both", expand = True)
 #=B=
 bbutt = Button(line1, text="B", command=bcom)
-bbutt.pack(fill="both", side=LEFT)
+bbutt.pack(side=LEFT, fill="both", expand = True)
 #=C=
 cbutt = Button(line1, text="C", command=ccom)
-cbutt.pack(fill="both", side=LEFT)
+cbutt.pack(side=LEFT, fill="both", expand = True)
 #=D=
 dbutt = Button(line1, text="D", command=dcom)
-dbutt.pack(fill="both", side=LEFT)
+dbutt.pack(side=LEFT, fill="both", expand = True)
 #=E=
 ebutt = Button(line1, text="E", command=ecom)
-ebutt.pack(fill="both", side=LEFT)
+ebutt.pack(side=LEFT, fill="both", expand = True)
 #=F=
 fbutt = Button(line1, text="F", command=fcom)
-fbutt.pack(fill="both", side=LEFT)
+fbutt.pack(side=LEFT, fill="both", expand = True)
 #=G=
 gbutt = Button(line2, text="G", command=gcom)
-gbutt.pack(fill="both", side=LEFT)
+gbutt.pack(side=LEFT, fill="both", expand = True)
 #=H=
 hbutt = Button(line2, text="H", command=hcom)
-hbutt.pack(fill="both", side=LEFT)
+hbutt.pack(side=LEFT, fill="both", expand = True)
 #=I=
 ibutt = Button(line2, text="I", command=icom)
-ibutt.pack(fill="both", side=LEFT)
+ibutt.pack(side=LEFT, fill="both", expand = True)
 #=J=
 jbutt = Button(line2, text="J", command=jcom)
-jbutt.pack(fill="both", side=LEFT)
+jbutt.pack(side=LEFT, fill="both", expand = True)
 #=K=
 kbutt = Button(line2, text="K", command=kcom)
-kbutt.pack(fill="both", side=LEFT)
+kbutt.pack(side=LEFT, fill="both", expand = True)
 #=L=
 lbutt = Button(line2, text="L", command=lcom)
-lbutt.pack(fill="both", side=LEFT)
+lbutt.pack(side=LEFT, fill="both", expand = True)
 #=M=
 mbutt = Button(line3, text="M", command=mcom)
-mbutt.pack(fill="both", side=LEFT)
+mbutt.pack(side=LEFT, fill="both", expand = True)
 #=N=
 nbutt = Button(line3, text="N", command=ncom)
-nbutt.pack(fill="both", side=LEFT)
+nbutt.pack(side=LEFT, fill="both", expand = True)
 #=O=
 obutt = Button(line3, text="O", command=ocom)
-obutt.pack(fill="both", side=LEFT)
+obutt.pack(side=LEFT, fill="both", expand = True)
 #=P=
 pbutt = Button(line3, text="P", command=pcom)
-pbutt.pack(fill="both", side=LEFT)
+pbutt.pack(side=LEFT, fill="both", expand = True)
 #=Q=
 qbutt = Button(line3, text="Q", command=qcom)
-qbutt.pack(fill="both", side=LEFT)
+qbutt.pack(side=LEFT, fill="both", expand = True)
 #=R=
 rbutt = Button(line3, text="R", command=rcom)
-rbutt.pack(fill="both", side=LEFT)
+rbutt.pack(side=LEFT, fill="both", expand = True)
 #=S=
 sbutt = Button(line4, text="S", command=scom)
-sbutt.pack(fill="both", side=LEFT)
+sbutt.pack(side=LEFT, fill="both", expand = True)
 #=T=
 tbutt = Button(line4, text="T", command=tcom)
-tbutt.pack(fill="both", side=LEFT)
+tbutt.pack(side=LEFT, fill="both", expand = True)
 #=U=
 ubutt = Button(line4, text="U", command=ucom)
-ubutt.pack(fill="both", side=LEFT)
+ubutt.pack(side=LEFT, fill="both", expand = True)
 #=V=
 vbutt = Button(line4, text="V", command=vcom)
-vbutt.pack(fill="both", side=LEFT)
+vbutt.pack(side=LEFT, fill="both", expand = True)
 #=W=
 wbutt = Button(line4, text="W", command=wcom)
-wbutt.pack(fill="both", side=LEFT)
+wbutt.pack(side=LEFT, fill="both", expand = True)
 #=X=
 xbutt = Button(line4, text="X", command=xcom)
-xbutt.pack(fill="both", side=LEFT)
+xbutt.pack(side=LEFT, fill="both", expand = True)
 #=Y=
 ybutt = Button(line5, text="Y", command=ycom)
-ybutt.pack(fill="both", side=LEFT)
+ybutt.pack(side=LEFT, fill="both", expand = True)
 #=Z=
 zbutt = Button(line5, text="Z", command=zcom)
-zbutt.pack(fill="both", side=LEFT)
+zbutt.pack(side=LEFT, fill="both", expand = True)
 #"Go back" button :
-backbutt = Button(mainframe, text="Go back", command=go_back)
-backbutt.pack(fill="both")
+backbutt = Button(leftframe, text="Go back", command=go_back)
+backbutt.pack(fill="both", expand = True)
 #"Restart" button :
-backbutt = Button(mainframe, text="Restart", command=run)
-backbutt.pack(fill="both")
+backbutt = Button(leftframe, text="Restart", command=run)
+backbutt.pack(fill="both", expand = True)
+#===
 #Don't pack this frame :
 mainframe.pack_forget()
+#hangman_drawing.pack_forget()
 #Don't pack the "game over !" label :
 gameover.pack_forget()
 #=========================================
@@ -306,7 +436,7 @@ gameover.pack_forget()
 
 #================commands================
 def loaddict():
-   filename = askopenfilename(title="Load your dictionnary",filetypes=[('Mibi dictionnaries','.mibidict'),('all files','.*')])
+   filename = askopenfilename(title="Load your dictionnary.",filetypes=[('Mibi dictionnaries','.mibidict'),('all files','.*')])
    dicoadress.set(filename)
    print(dicoadress.get())
 def play():
@@ -315,13 +445,13 @@ def play():
    if dictadress != "":
       run()
    else:
-      showerror("Error", "Please choose a dictionnary")
+      showerror("Error", "Please choose a dictionnary.\nError number 1")
 #==========================================
 choosedictionnarybutt = Button(chooseframe, text="Load a dictionnary", command=loaddict)
-choosedictionnarybutt.pack(fill="both")
+choosedictionnarybutt.pack(fill="both", expand = True)
 #---
 launchbutt = Button(chooseframe, text="Play !", command=play)
-launchbutt.pack(fill="both")
+launchbutt.pack(fill="both", expand = True)
 #==========================================
 #==========================================
 #==========================================
